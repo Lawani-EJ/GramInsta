d7de3046bdb6620e6530df5ff223bdaa4143283c
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-19 12:44:19 +0100

    Tailwind Usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+// import './App.css'
+import { Heading } from "@chakra-ui/react"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Heading size="6xl"> Commented the Previous css and i&apos;ve added chakra UI with some assets and configured the alias at the vite configuration 🥲😁😗 </Heading>
     </>
   )
 }
