fa7856b91eaa84b41f5d0fe4d9d1792852f3ff3c
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-20 23:53:28 +0100

    Created Home Page

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import { Heading } from "@chakra-ui/react"
+
+const Homepage = () => {
+  return (
+    <div>
+          <Heading size="6xl"> Sike This is the Homepage </Heading>
+    </div>
+  )
+}
+
+export default Homepage
