5b9d47b2ff47ce39a4cb3dfdcf73d11a7ab4ae0e
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-20 23:53:00 +0100

    Created Auth Page

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import { Heading } from '@chakra-ui/react'
+
+const AuthPage = () => {
+  return (
+    <div>
+        <Heading>This is the Auth Page</Heading>      
+    </div>
+  )
+}
+
+export default AuthPage
